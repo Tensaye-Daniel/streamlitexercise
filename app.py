3de9b27ec16369385d1d6f2c5aac2e71af6814ae
Tensaye-Daniel <tensayedaniel77@gmail.com>
2026-09-19 12:28:10 +0300

    update Streamlit app configuration and enhance header section

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 # find the https://www.webfx.com/tools/emoji-cheat-sheet/ the icons
-st.set_page_config(page_title="Streamlit webpage", page_icon=":💀:")
+st.set_page_config(page_title="Streamlit webpage", page_icon=":tada:", layout="wide")
+#---HEADER SECTION ----
+with st.container():
+    st.subheader("HI, I am tensaye :wave: ")
+    st.title("A software enginer from Ethiopia")
+    st.write("I am passionate about finding ways to use python and VBA to be more effective and efficient in business settings.")
+    st.write("[Learn more >](https://portfolio2-six-flax.vercel.app/)")
+
+#--- WHAT I DO ---
+
